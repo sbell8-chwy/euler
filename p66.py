7a1dea8723374ca0aa08d3ba4cba04a8ecbb62dc
Steven Bell <bell.steven@gmail.com>
2013-02-14 15:50:14 -0800

    simpler p66

--- p66.py
+++ p66.py
@@ -2,27 +2,19 @@ import euler_tools as et
 import math
 
 def xdy(max_d):
-    x = 1.0
     y = 1.0
     large_x = 0
-    for d in range(max_d, max_d + 1):
+    for d in range(2, max_d + 1):
         if et.is_sqr(d):
             continue
-        x2 = x**2
         y2 = d * (y**2)
-        a = x2 - y2
-        while a != 1:
-            if a > 1:
-                y += 1.0
-                y2 = d * (y**2)
-            if a < 1:
-                x = next_x(y2)
-                x2 = x**2
-            a = x2 - y2
-            print '%d - %d = %d' % (x, y, a)
+        while not et.is_sqr(y2 + 1):
+            y += 1.0
+            y2 = d * (y**2)
+        x = math.sqrt(y2 + 1)
         if x > large_x:
             large_x = x
-        x, y = 1.0, 1.0
+        y = 1.0
     return large_x
 
 
